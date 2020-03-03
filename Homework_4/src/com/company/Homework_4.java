99b3a17bb798a720ef010cdbb8fbbd440210a7cb
Denis <47885411+birdman93@users.noreply.github.com>
2020-03-03 16:27:13 +0300

    start

--- Homework_4/src/com/company/Homework_4.java
+++ Homework_4/src/com/company/Homework_4.java
@@ -1,6 +1,20 @@
 package com.company;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Homework_4 {
+
+    public static final int SIZE = 3;
+    public static final int DOT_TO_WIN = 3;
+
+    public static final char DOT_EMPTY = '▪';
+    public static final char DOT_X = 'X';
+    public static final char DOT_0 = '0';
+
+    public static char [] map;
+    public static Scanner scanner = new Scanner(System.in);
+    public static Random random = new Random();
 
     public static void main(String[] args) {
 
